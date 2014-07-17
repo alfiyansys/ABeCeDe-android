e2aea7db3dd5680d663fa50cbb8827ccf1112500
M Alfiyan S <alfiyan.syamsuddin@gmail.com>
2014-07-17 20:37:33 +0700

    Add : character intent determiner + unmod data ISVO

--- ABeCeDe-android/src/com/yammy/abecede/Huruf.java
+++ ABeCeDe-android/src/com/yammy/abecede/Huruf.java
@@ -1,20 +1,31 @@
 package com.yammy.abecede;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.TextView;
 
+@SuppressLint("DefaultLocale")
 public class Huruf extends Activity {
-	Bundle bundle;
-	int huruf;
+	Bundle bundle;	
+	String strhuruf;
+	TextView hurufView;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		int hurufNum;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_huruf);
+		hurufView = (TextView) this.findViewById(R.id.teksGede);
 		bundle = getIntent().getExtras();
-		huruf = bundle.getInt("huruf");
-		huruf++;
-		Toast.makeText(getApplicationContext(), huruf+"", Toast.LENGTH_LONG).show();
+		hurufNum = bundle.getInt("huruf");
+		hurufView.setText(determineHuruf(hurufNum));
+	}
+	
+	private String determineHuruf(int i){
+		String hurufs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String hurufk = hurufs.charAt(i)+"";
+		hurufk = hurufk.toLowerCase();
+		return hurufs.charAt(i)+hurufk;
 	}
 }
