7fbb1cbd5bd57968bf03983c65b3d2606a17ec0f
M Alfiyan S <alfiyan.syamsuddin@gmail.com>
2014-08-18 21:53:08 +0700

    Add : Text To Speech

--- ABeCeDe-android/src/com/yammy/abecede/Huruf.java
+++ ABeCeDe-android/src/com/yammy/abecede/Huruf.java
@@ -1,31 +1,44 @@
 package com.yammy.abecede;
 
+import java.util.Locale;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 
 import com.yammy.abecede.db.MySQLHelper;
 
 @SuppressLint("DefaultLocale")
-public class Huruf extends Activity {
+public class Huruf extends Activity implements OnInitListener {
 	Bundle bundle;	
 	String strhuruf;
 	TextView hurufView;
 	ListView kataList;
 	private MySQLHelper dbHelper;
 	protected ListAdapter adapter;
+	Cursor mainCursor;
+	TextToSpeech tts;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		int hurufNum;
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_huruf);
+		
+		tts = new TextToSpeech(this,this);
+		
 		hurufView = (TextView) this.findViewById(R.id.teksGede);
 		bundle = getIntent().getExtras();
 		hurufNum = bundle.getInt("huruf");
@@ -33,7 +46,17 @@ public class Huruf extends Activity {
 		
 		this.kataList = (ListView) this.findViewById(R.id.LV1);
         this.kataList.setSelected(true);
-        //onclick item listener belum
+        this.kataList.setOnItemClickListener(new OnItemClickListener(){
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				mainCursor.moveToPosition(arg2);
+				if(tts != null){
+					if (!tts.isSpeaking()){
+						tts.speak(mainCursor.getString(1), TextToSpeech.QUEUE_FLUSH, null);
+					}
+				}
+			}
+        });
         dbHelper = new MySQLHelper(this);
         this.refreshList(determineHuruf(hurufNum,1));
 	}
@@ -44,6 +67,7 @@ public class Huruf extends Activity {
 			Cursor cursor = db.rawQuery("select * from benda where nama like '"+hr+"%'", null);
 			adapter = new android.support.v4.widget.SimpleCursorAdapter(this, R.layout.view_kata, cursor, new String[] {"nama","eja"}, new int[] {R.id.namaBendaItem,R.id.ejaBendaItem}, 0);
 			kataList.setAdapter(adapter);
+			mainCursor = cursor;
 		}catch(Exception e){
 			Log.d("Exception", e.toString());
 		}
@@ -58,5 +82,24 @@ public class Huruf extends Activity {
 		}else{
 			return hurufs.charAt(i)+"";
 		}
+	}
+
+	@Override
+	public void onInit(int status) {
+		// TODO Auto-generated method stub
+		if(status == TextToSpeech.SUCCESS){
+			tts.setLanguage(Locale.getDefault());
+		}else{
+			Toast.makeText(this, "TTS error", Toast.LENGTH_SHORT).show();
+		}
+	}
+	
+	@Override
+	protected void onDestroy(){
+		if(tts != null){
+			tts.stop();
+			tts.shutdown();
+		}
+		super.onDestroy();
 	}
 }
